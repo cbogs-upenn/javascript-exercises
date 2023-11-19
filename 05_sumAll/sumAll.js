66575bef339459b7542ed9fea45fe12d844d1a90
Christopher Bogs <cbogs@upenn.edu>
2023-11-18 23:01:30 -0500

    Complete Lesson 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
 
+    let sumTotal = 0;
+
+    // test suite wants us to take the args in either order
+    if (startInt > endInt){
+        let tempInt = startInt;
+        startInt = endInt;
+        endInt = tempInt;
+    }
+
+    //test suite wants negative numbers to return 'ERROR'
+    if ((startInt < 0)||(endInt < 0)){
+        return "ERROR";
+    }
+
+    //test suite wants non-number parameters to return 'ERROR'
+    if ((typeof startInt !== "number")||(typeof endInt !== "number")){
+        return "ERROR";
+    }
+
+    for (let i = startInt; i <= endInt; i++){
+
+        sumTotal += i;
+    }
+
+    return sumTotal;
 };
 
 // Do not edit below this line
