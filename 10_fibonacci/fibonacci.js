0a91e7632a29ab59aead1a1aaf65b4c4dea8484f
Christopher Bogs <cbogs@upenn.edu>
2023-11-20 22:47:46 -0500

    Complete Lesson 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function(targetNum) {
+
+    //ensure it's a number not a string
+    if (typeof targetNum !== "number"){
+        targetNum = Number(targetNum);
+    }
+
+    //handle special cases
+
+    if (targetNum < 0){
+        return "OOPS";
+    }
+
+    if (targetNum === 1){
+        return 1;
+    }
+    
+    // and then away we go
+
+    let firstNumber = 0;
+    let nextNumber = 1;
+    let sum = 0;
+
+    for (let i = 1; i < targetNum; i++){
+
+        sum = firstNumber + nextNumber;
+        firstNumber = nextNumber;
+        nextNumber = sum;
+                
+    }
+
+    return sum;
 
 };
 
