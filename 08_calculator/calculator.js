7a2c49c68e37ea3ff6ff0393231a77dd48f12a94
Christopher Bogs <cbogs@upenn.edu>
2023-11-20 21:13:27 -0500

    Complete Lesson 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,41 @@
-const add = function() {
+const add = function(a, b) {
+  return a+b;	
+};
+
+const subtract = function(a, b) {
+  return a-b;
+};
+
+const sum = function(inputArray) {
+  if (Array.isArray(inputArray)){
+   const output = inputArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+   return output;
+  } else 
+  {return 0;} 
+};
+
+const multiply = function(inputArray) {
+  if (Array.isArray(inputArray)){
+    const output = inputArray.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+    return output;
+   } else 
+   {return 0;} 
+
+};
+
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(a) {
+  let init = 1;
+  while (a > 0){
+    init *= a;
+    a--;
+  }
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  return init;
 	
 };
 
