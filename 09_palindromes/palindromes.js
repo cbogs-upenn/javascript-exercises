ed97d14b548e89699e801f548bad59b185e582d8
Christopher Bogs <cbogs@upenn.edu>
2023-11-20 22:03:47 -0500

    Complete Lesson 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
-};
+    //clean up input (remove punctuation and spaces)
+    cleanPhrase = input.trim().toLowerCase().replace(/[^a-z0-9]/g, "");
+    console.log(cleanPhrase);
+    
+    //trim and lowercase input and split it into an array
+    let testPhrase = cleanPhrase.split("");
+
+    let first = "";
+    let last = "";
+
+    do {
+        // shift the first element and pop the last
+        first = testPhrase.shift();
+        last = testPhrase.pop();
+        console.log("Testing: " + first + " and " + last);
+
+        if (first !== last) {
+            return false;
+
+        } 
+    } while (testPhrase.length > 1);
+        
+        return true;
+    }
+    
 
 // Do not edit below this line
 module.exports = palindromes;
