f443c1039075f9c724c260108940caab5b5cf239
Christopher Bogs <cbogs@upenn.edu>
2023-11-21 20:51:25 -0500

    Complete Lesson 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    const newArray = books.map(getTitle);
+    return newArray;
 };
+
+function getTitle(books){
+
+   return books.title;
+
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
