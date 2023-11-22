78c721e7b13b8d76225fe804732d76933f4839c2
Christopher Bogs <cbogs@upenn.edu>
2023-11-21 22:41:26 -0500

    Complete Lesson 12 (OMG)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    const newArray = array.reduce(isOldest);
+    console.log(newArray);
+    return newArray;
+        
+        
 };
+
+function isOldest(oldest, currentPerson){
+
+    const currentAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+    const oldAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    
+    if (currentAge > oldAge){
+        return currentPerson;
+    }
+    else {
+        return oldest;
+    }
+
+    
+}
+
+function getAge(death, birth){
+
+    if (!death) {
+
+        death = new Date().getFullYear();
+
+    }
+        return death-birth;
+ 
+    }
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
